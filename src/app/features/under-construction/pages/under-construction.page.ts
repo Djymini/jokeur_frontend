7827c0426d09a7ddc6bf386a077992ab2d0a14fa
Mawele <mbizi.kevin@gmail.com>
2026-01-22 10:19:59 +0100

    install prettier

--- src/app/features/under-construction/pages/under-construction.page.ts
+++ src/app/features/under-construction/pages/under-construction.page.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
-import { UnderConstructionComponent } from "../components/under-construction.component/under-construction.component";
+import { UnderConstructionComponent } from '../components/under-construction.component/under-construction.component';
 
 @Component({
   selector: 'app-under-construction.page',
   imports: [UnderConstructionComponent],
-  template: `
-    <app-under-construction></app-under-construction>
-  `,
+  template: ` <app-under-construction></app-under-construction> `,
   styles: ``,
 })
-export default class UnderConstructionPage {
-
-}
+export default class UnderConstructionPage {}
