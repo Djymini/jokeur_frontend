e6059236e7f769956b3e5d2a4ad3a5dbc55ecc81
sarakadimi37 <sarakadimi37@gmail.com>
2026-02-04 11:06:24 +0100

    creation of sidebar, header and footer layout

---
+++ src/app/core/layout/sidebar/sidebar.component.ts
@@ -0,0 +1,34 @@
+import { Component, input, signal } from '@angular/core';
+
+type MenuItem = {
+  id: string;
+  label: string;
+  icon: string;
+  route: string;
+  badge?: number;
+}
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss',
+})
+export default class SidebarComponent {
+
+  collapsed = input<boolean>(false);
+  activeRoute = input<string>('/dashboard');
+
+  // Signals internes
+  menuItems = signal<MenuItem[]>([
+    { id: 'dashboard', label: 'Tableau de bord', icon: 'dashboard', route: '/dashboard' },
+    { id: 'animals', label: 'Mes animaux', icon: 'pets', route: '/animals'},
+    { id: 'calendar', label: 'Agenda', icon: 'calendar_today', route: '/calendar' },
+    { id: 'settings', label: 'Paramètres', icon: 'settings', route: '/settings' }
+  ]);
+
+  // Méthodes
+  selectMenu(): void {
+    // todo
+  }
+}
