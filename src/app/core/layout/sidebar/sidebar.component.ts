bd0249d8a5ea6f1f1adfbb1f61d1801f9fbdad52
sarakadimi37 <sarakadimi37@gmail.com>
2026-02-19 11:00:31 +0100

    correction of sidebar

--- src/app/core/layout/sidebar/sidebar.component.ts
+++ src/app/core/layout/sidebar/sidebar.component.ts
@@ -1,26 +1,21 @@
 import { Component, input, signal } from '@angular/core';
 import { MenuItemModel } from '@/internal-shared/domaine/MenuItem.model';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
   collapsed = input<boolean>(false);
-  activeRoute = input<string>('/dashboard');
 
-  // Signals internes
+
   menuItems = signal<MenuItemModel[]>([
     { id: 'dashboard', label: 'Tableau de bord', icon: 'dashboard', path: '/dashboard' },
     { id: 'animals', label: 'Mes animaux', icon: 'pets', path: '/construction' },
     { id: 'calendar', label: 'Agenda', icon: 'calendar_today', path: '/construction' },
     { id: 'settings', label: 'Paramètres', icon: 'settings', path: '/construction' },
   ]);
-
-  // Méthodes
-  selectMenu(): void {
-    // todo
-  }
 }
