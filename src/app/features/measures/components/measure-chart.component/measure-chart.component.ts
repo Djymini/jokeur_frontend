ccc85d22582ab102d085e35d3e77edde6f96f718
Mawele <mbizi.kevin@gmail.com>
2026-02-23 16:35:36 +0100

    fix the display chart

--- src/app/features/measures/components/measure-chart.component/measure-chart.component.ts
+++ src/app/features/measures/components/measure-chart.component/measure-chart.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, input } from '@angular/core';
+import {  Component, effect, input, signal } from '@angular/core';
 import { ChartManager } from '@/features/measures/utils/chartManager';
 import { ChartMeasureModel } from '@/internal-shared/models/chartMeasure.model';
 import { MeasureModel } from '@/features/measures/models/measureModel';
@@ -9,25 +9,43 @@ import { MeasureModel } from '@/features/measures/models/measureModel';
   templateUrl: './measure-chart.component.html',
   styleUrl: './measure-chart.component.scss',
 })
-export class MeasureChartComponent implements AfterViewInit {
+export class MeasureChartComponent {
   title = input.required<string>();
   type = input.required<string>();
   mesures = input.required<MeasureModel[]>();
-  public chart: any;
 
-  ngAfterViewInit(): void {
-    const label: string[] = [];
-    const data: string[] = [];
+  public chart = signal<any>(null);
 
-    for (const measure of this.mesures()) {
-      label.push(measure.creationDate);
-      data.push(measure.value.toString());
-    }
+  constructor() {
+    setTimeout(() => {
+      this._createChart();
+    }, 1000);
+
+    effect(() => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const data:MeasureModel[] = this.mesures();
+      const currentChart = this.chart();
+
+      if (currentChart) {
+        this._updateChart(currentChart);
+      }
+    });
+  }
+
+  private _createChart(): void {
     const dataMeasure: ChartMeasureModel = {
       name: this.title(),
-      labels: label,
-      data: data,
+      labels: this.mesures().map(m => m.creationDate),
+      data: this.mesures().map(m => m.value.toString())
     };
-    this.chart = ChartManager.createChart(this.type(), 'line', dataMeasure);
+
+    const newChart = ChartManager.createChart(this.type(), 'line', dataMeasure);
+    this.chart.set(newChart);
+  }
+
+  private _updateChart(chartInstance: any): void {
+    chartInstance.data.labels = this.mesures().map(m => m.creationDate);
+    chartInstance.data.datasets[0].data = this.mesures().map(m => m.value.toString());
+    chartInstance.update();
   }
 }
