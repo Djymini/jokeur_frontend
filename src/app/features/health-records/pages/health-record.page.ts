6171ebc522163c60cc1f803f5fe96e028ff1a11f
Mawele <mbizi.kevin@gmail.com>
2026-02-27 09:27:46 +0100

    finish vaccine

--- src/app/features/health-records/pages/health-record.page.ts
+++ src/app/features/health-records/pages/health-record.page.ts
@@ -1,18 +1,19 @@
 import { Component, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { HealthRecordFacade } from '@/features/health-records/services/health-record.facade';
 import { HealthRecordHeaderComponent } from '@/features/health-records/components/health-record-header.component/health-record-header.component';
 import { HealthRecordSectionComponent } from '@/features/health-records/components/health-record-section.component/health-record-section.component';
+import { ZardButtonComponent } from '@/shared/components/button';
 
 @Component({
   selector: 'app-health-record.page',
-  imports: [HealthRecordHeaderComponent, HealthRecordSectionComponent],
+  imports: [HealthRecordHeaderComponent, HealthRecordSectionComponent, ZardButtonComponent],
   template: `
     <div class="button-container">
-      <button z-button zSize="lg" zType="link">
+      <z-button z-button zSize="lg" zType="link" (click)="returnToDashboard()">
         <span class="material-icons cursor-pointer">arrow_back</span>
         Retour à mon tableau de bord
-      </button>
+      </z-button>
     </div>
     <app-health-record-header [healthRecord]="healthRecord"></app-health-record-header>
     <app-health-record-section [healthRecord]="healthRecord"></app-health-record-section>
@@ -45,4 +46,10 @@ export default class HealthRecordPage {
   private healthRecordFacade = inject(HealthRecordFacade);
 
   healthRecord = this.route.snapshot.data['healthRecord'];
+
+  constructor(private router: Router) {}
+
+  returnToDashboard() {
+    this.router.navigate(['/dashboard']);
+  }
 }
