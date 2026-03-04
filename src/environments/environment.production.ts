eaae716feb5106eaf9d7eb51e02ab1d9b909f648
Mawele <mbizi.kevin@gmail.com>
2026-03-04 16:35:29 +0100

    fix the url bug

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   name: 'production',
-  apiUrl: 'http://api.jokeur.ashleydev.fr',
+  apiUrl: 'https://api.jokeur.ashleydev.fr',
 };
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   name: 'staging',
-  apiUrl: 'http://staging-api.jokeur.ashleydev.fr',
+  apiUrl: 'https://staging-api.jokeur.ashleydev.fr',
 };
