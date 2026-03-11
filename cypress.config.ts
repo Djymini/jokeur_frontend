9423ec52805a5c44366256b10f3da2fe636673d1
Ashley <laboryashley@gmail.com>
2026-03-11 15:24:53 +0100

    restoring the file to its original state

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,10 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:4200',
-    specPattern: 'src/tests/e2e/use-cases/**/*.cy.ts',
-    supportFile: 'src/tests/e2e/support/e2e.ts',
-    fixturesFolder: 'src/tests/e2e/fixtures',
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
