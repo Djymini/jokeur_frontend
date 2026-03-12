d3a02696d3710257819d26aeb0bdca71a84bba9c
Ashley <laboryashley@gmail.com>
2026-03-12 11:33:10 +0100

    minor changes

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,10 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:4200",
-    supportFile: "src/tests/e2e/support/e2e.ts",
-    specPattern: "src/tests/e2e/use-cases/**/*.cy.ts",
-    fixturesFolder: "src/tests/e2e/fixtures",
+
     setupNodeEvents(on, config) {},
   },
 });
