4c3607da731f390bf93bca26177195b5daa919c2
yol97 <yol974@hotmail.com>
2026-03-17 09:52:11 +0100

    toasts fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HealthRecordFormBootstrapService } from '@/features/health-records/services/health-record-form-bootstrap.service';
+import { NgxSonnerToaster } from 'ngx-sonner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgxSonnerToaster],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
