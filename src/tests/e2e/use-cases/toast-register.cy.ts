f9a9cf82170de9465071da9589e5088ca7303e04
yol97 <yol974@hotmail.com>
2026-03-18 16:37:28 +0100

    add register toasts test

--- src/tests/e2e/use-cases/toast-register.cy.ts
+++ src/tests/e2e/use-cases/toast-register.cy.ts
@@ -1,29 +1,55 @@
-// import { environment } from '../../../environments/environment';
+import { environment } from '../../../environments/environment';
 
 describe('Toasts - Register', () => {
-  it('affiche le toast de succès après inscription et redirige vers /login', () => {
-    cy.intercept('POST', '**/auth/register', {
+  const fillRegisterForm = (u: any) => {
+    cy.get('#name').should('not.be.disabled').type(u.name);
+    cy.get('#firstname').should('not.be.disabled').type(u.firstname);
+    cy.get('#phone').should('not.be.disabled').type('0612345678');
+    cy.get('#email').should('not.be.disabled').type(u.email);
+
+    cy.get('#password').should('not.be.disabled').type('Password123!');
+    cy.get('#confirmPassword').should('not.be.disabled').type('Password123!');
+    cy.get('#acceptCGU').check({ force: true });
+  };
+
+  it('Displays successful toast after register then navigate to login', () => {
+    cy.intercept('POST', `${environment.apiUrl}auth/register`, {
       statusCode: 200,
-      body: { message: 'Inscription réussie.' },
+      body: { message: 'Utilisateur inscrit avec succès.' },
     }).as('registerOk');
 
     cy.visit('/register');
 
-    cy.get('#name').should('not.be.disabled').type('Doe');
-    cy.get('#firstname').should('not.be.disabled').type('John');
-    cy.get('#phone').should('not.be.disabled').type('0612345678');
-    cy.get('#email').should('not.be.disabled').type('john.doe@mail.com');
-    cy.get('#password').should('not.be.disabled').type('Password123!');
-    cy.get('#confirmPassword').should('not.be.disabled').type('Password123!');
-    cy.get('#acceptCGU').check({ force: true });
+    cy.fixture('auth.json').then((u) => {
+      fillRegisterForm(u);
+    });
 
     cy.contains('button', "S'enregistrer").click();
     cy.wait('@registerOk');
 
-    // si tu rediriges vers /login
     cy.location('pathname', { timeout: 10000 }).should('eq', '/login');
-
-    // toast (exist = stable)
     cy.get('[data-sonner-toast]').contains('Inscription réussie.').should('exist');
+  });
+
+  it('Displays error toast when email already used (409)', () => {
+    cy.intercept('POST', `${environment.apiUrl}auth/register`, {
+      statusCode: 409,
+      body: {
+        error: 'USER_EMAIL_ALREADY_USED',
+        message: `Cet email est déjà utilisé : test@test.com`,
+      },
+    }).as('registerEmail409');
+
+    cy.visit('/register');
+
+    cy.fixture('auth.json').then((u) => {
+      fillRegisterForm(u);
+    });
+
+    cy.contains('button', "S'enregistrer").click();
+    cy.wait('@registerEmail409');
+
+    cy.location('pathname').should('eq', '/register');
+    cy.get('[data-sonner-toast]').contains('Cet email est déjà utilisé').should('exist');
   });
 });
